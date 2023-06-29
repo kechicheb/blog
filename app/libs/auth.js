6d98b7d2adb4398b6720172461d7718494fcc29e
Ahmed Kechicheb <ahmedkechicheb13@gmail.com>
2023-06-29 16:30:46 +0100

    some changes

--- app/libs/auth.js
+++ app/libs/auth.js
@@ -2,7 +2,6 @@ import jwt from "jsonwebtoken";
 const secret = process.env.SECRET;
 
 export function generateToken(payload) {
-  console.log(jwt.sign(payload, secret, { expiresIn: "1h" }));
   return jwt.sign(payload, secret, { expiresIn: "1h" });
 }
 
