b4b67b38c41cc255a3efb702b0f6ef4f407b8c7a
Ahmed Kechicheb <ahmedkechicheb13@gmail.com>
2023-06-29 13:00:41 +0100

    post & put

--- app/api/post/route.js
+++ app/api/post/route.js
@@ -1,7 +1,7 @@
 import Post from "@/models/Post";
 import { NextResponse } from "next/server";
 export async function GET(req) {
-  const { id } = req.query;
+  const {query: id } =await req.json();
   if (id) {
     const postDoc = await Post.findById(id).populate("author", ["username"]);
     return NextResponse.json(postDoc);
@@ -13,4 +13,55 @@ export async function GET(req) {
         .limit(20)
     );
   }
+}
+
+export async function POST(req){
+  const {originalname,path} = req.file;
+  const parts = originalname.split('.');
+  const ext = parts[parts.length - 1];
+  const newPath = path+'.'+ext;
+  fs.renameSync(path, newPath);
+
+  const {token} = req.cookies;
+  jwt.verify(token, secret, {}, async (err,info) => {
+    if (err) throw err;
+    const {title,summary,content} = req.body;
+    const postDoc = await Post.create({
+      title,
+      summary,
+      content,
+      cover:newPath,
+      author:info.id,
+    });
+    res.json(postDoc);
+  });
+}
+export async function PUT(req){
+  let newPath = null;
+  if (req.file) {
+    const {originalname,path} = req.file;
+    const parts = originalname.split('.');
+    const ext = parts[parts.length - 1];
+    newPath = path+'.'+ext;
+    fs.renameSync(path, newPath);
+  }
+
+  const {token} = req.cookies;
+  jwt.verify(token, secret, {}, async (err,info) => {
+    if (err) throw err;
+    const {id,title,summary,content} = req.body;
+    const postDoc = await Post.findById(id);
+    const isAuthor = JSON.stringify(postDoc.author) === JSON.stringify(info.id);
+    if (!isAuthor) {
+      return res.status(400).json('you are not the author');
+    }
+    await postDoc.update({
+      title,
+      summary,
+      content,
+      cover: newPath ? newPath : postDoc.cover,
+    });
+
+    res.json(postDoc);
+  });
 }
